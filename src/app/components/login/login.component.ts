22e8350dfa36a5962f48a03ab079837ccfda81c1
Daniel <d.labrasilva@gmail.com>
2025-02-12 20:21:27 -0500

    feat: :sparkles: Added Reactive Form Validation

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,22 +1,50 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import ValidateForm from '../../helpers/validators';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterModule],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule], //step 1: Add reactive forms module to validate the login form
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
   type: string = 'password';
   isText: boolean = false;
   eyeIcon: string = 'fa-eye-slash';
+  loginForm!: FormGroup; //step 2: Create a form group for the login form
+
+  constructor(private formBuilder: FormBuilder) { } //step 3: Inject the form builder service
+
+  ngOnInit(): void {
+    //step 4: Create the login form with the form builder service
+    this.loginForm = this.formBuilder.group({
+      username:['', Validators.required],
+      password:['', Validators.required]
+    });
+  } 
 
   hideShowPassword() {
     this.isText = !this.isText;
     this.isText ? this.type = 'text' : this.type = 'password';
     this.isText ? this.eyeIcon = 'fa-eye' : this.eyeIcon = 'fa-eye-slash';
   }
+
+  onSubmit() {
+    if(this.loginForm.valid) {
+      console.log(this.loginForm.value);
+    }else{
+      //throw error
+      ValidateForm.validateAllFormFields(this.loginForm);
+      alert('Please fill in the required fields');
+    }
+  }
+
+
+
 }
--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,21 +1,47 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import ValidateForm from '../../helpers/validators';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [RouterModule],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
   type: string = 'password';
   isText: boolean = false;
   eyeIcon: string = 'fa-eye-slash';
+  signupForm!: FormGroup;
+  
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.signupForm = this.formBuilder.group({
+      firstname:['', Validators.required],
+      lastname:['', Validators.required],
+      email:['', Validators.required],
+      username:['', Validators.required],
+      password:['', Validators.required]
+    });
+  }
 
   hideShowPassword() {
     this.isText = !this.isText;
     this.isText ? this.type = 'text' : this.type = 'password';
     this.isText ? this.eyeIcon = 'fa-eye' : this.eyeIcon = 'fa-eye-slash';
   }
+
+  onSubmit() {
+      if(this.signupForm.valid) {
+        console.log(this.signupForm.value);
+      }else{
+        //throw error
+        ValidateForm.validateAllFormFields(this.signupForm);
+       // alert('Please fill in the required fields');
+      }
+    }
 }
